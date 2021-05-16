d663581c361328dd0b9bcf73f6235b3734aa9956
yoon-jo hwang <mymelody112266@gmail.com>
2021-05-16 17:18:36 +0900

    2nd commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Soyeon Park on 2021/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        
+        
+        
+    }
+
+
+}
+
